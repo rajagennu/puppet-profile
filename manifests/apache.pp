3e3efa26fd7fda01db2bc2fce46d1e28871ef9a1
root <root@rgenupula1.mylabserver.com>
2017-10-06 04:14:33 +0000

    added web profile

---
+++ manifests/apache.pp
@@ -0,0 +1,17 @@
+class profile::apache(
+  Boolean $default_vhost = false,
+  Integer $port = 80,
+  String $docroot = '/var/www/test',
+  )
+{
+ class {'::apache':
+  default_vhost => $default_vhost,
+ }
+
+  ::apache::vhost { 'rgenupula2.mylabserver.com':
+  port    => $port,
+  docroot => $docroot,
+ }
+
+}
+
