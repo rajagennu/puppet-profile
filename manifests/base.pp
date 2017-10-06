2450314638634277975711865cf7bfc275e0a245
root <root@rgenupula1.mylabserver.com>
2017-10-06 03:44:58 +0000

    Simple profile with NTP and Selinux

---
+++ manifests/base.pp
@@ -0,0 +1,20 @@
+class profile::base(
+  $ntp_servers = [
+    '0.ubuntu.pool.mtp.org',
+    '1.ubuntu.pool.ntp.org',
+
+],
+) {
+  include ::ssh
+
+class { '::ntp': 
+  servers => $ntp_servers,
+}
+
+if $facts['os']['family'] == 'RedHat' 
+{
+ include ::profile::selinux
+}
+}
+
+  
