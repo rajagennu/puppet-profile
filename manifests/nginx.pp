c9608d8044bf20fedfd9a33749c58d3f6f891714
root <root@rgenupula1.mylabserver.com>
2017-11-02 05:11:37 +0000

    updating profile as per hiera data

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -1,8 +1,13 @@
 class profile::nginx {
 
 	include ::nginx
+        $vhosts.each |$vhost| {
+            ::nginx::vhost {$vhost['fqdn']:
+                port           => $vhost['port'],
+                server_aliases => $vhost['server_aliases'],
 
-	::nginx::vhosts {$facts['fqdn']:
 
 	}
+}
+
 }
