03c5799a8562752e6ae049d22f552bdab3aa0261
root <root@rgenupula1.mylabserver.com>
2017-11-02 06:04:30 +0000

    fixing errors

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -6,7 +6,7 @@ class profile::nginx(
 	include ::nginx
        
         $vhosts.each |$vhost| {
-            ::nginx::vhost {$vhost['fqdn']:
+            ::nginx::vhosts {$vhost['fqdn']:
                 port           => $vhost['port'],
                 server_aliases => $vhost['server_aliases'],
 
